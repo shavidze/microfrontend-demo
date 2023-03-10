ea0cdf6ef57b937ae708e560344fbb414d07d55e
Saba-Shavidze <saba@ancient.gg>
2023-03-10 23:58:55 +0400

    load data/ add store with functionalities

--- growlers/src/components/Search.tsx
+++ growlers/src/components/Search.tsx
@@ -9,20 +9,33 @@ import {
   SliderThumb,
   Box,
 } from "@chakra-ui/react";
+import { useProxy } from "valtio/utils";
 
+import store, { setSearchText, setAlcoholLimit } from "../store";
 import { MFE_BORDER } from "../constants";
 
 const Search = () => {
+  const { searchText, alcoholLimit } = useProxy(store);
   return (
     <Box border={MFE_BORDER}>
       <FormControl id="search">
         <FormLabel>Search</FormLabel>
-        <Input type="text" />
+        <Input
+          type="text"
+          value={searchText}
+          onChange={(evt) => setSearchText(evt.target.value)}
+        />
       </FormControl>
 
       <FormControl id="alcohol">
         <FormLabel>Alcohol</FormLabel>
-        <Slider colorScheme="pink" defaultValue={3} min={0} max={17}>
+        <Slider
+          colorScheme="pink"
+          defaultValue={alcoholLimit}
+          onChange={(v) => setAlcoholLimit(v)}
+          min={0}
+          max={17}
+        >
           <SliderTrack>
             <SliderFilledTrack />
           </SliderTrack>
--- growlers/src/components/Taps.tsx
+++ growlers/src/components/Taps.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-
+import { useProxy } from "valtio/utils";
 import BeverageCard from "./BeverageCard";
 import { MFE_BORDER } from "../constants";
-
+import store from "../store";
 const Taps = () => {
+  const { filteredTaps } = useProxy(store);
   return (
     <Box border={MFE_BORDER}>
-      {[].map((beverage: any) => (
+      {filteredTaps.map((beverage) => (
         <BeverageCard
           key={[beverage.producerName, beverage.beverageName].join("")}
           beverage={beverage}
